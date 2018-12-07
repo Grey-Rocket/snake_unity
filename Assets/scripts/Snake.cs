1575fe864d7f04de713c360c97d0c86f9b01ab2d
whiterocket <aljaz.podgornik@t-2.net>
2018-12-08 00:08:27 +0100

    a "finished" prototype

--- Assets/scripts/Snake.cs
+++ Assets/scripts/Snake.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEditor;
 
 public class Snake : MonoBehaviour
 {
@@ -18,10 +19,16 @@ public class Snake : MonoBehaviour
         }
     }
 
-    Queue snqueue;
     public Tilemap snake;
+    public Tilemap fruitTable;
+
     public Tile snakePart;
+    public Tile edge;
+    public Tile fruit;
+
+    Queue snqueue;
     Pozicija next;
+    string direction;
 
     // Use this for initialization
     void Start()
@@ -31,43 +38,103 @@ public class Snake : MonoBehaviour
 
         next = new Pozicija(-1, -2);
 
+        direction = "down";
+
         snqueue.Enqueue(new Pozicija(-1, 0));
-        //elem = new Pozicija(-1, -1);
         snqueue.Enqueue(new Pozicija(-1, -1));
-        /*
-        Debug.Log(((Pozicija)snqueue.Peek()).y);
-        snqueue.Dequeue();
-        Debug.Log(((Pozicija)snqueue.Peek()).y);
-        */
-        /*
-        for (int i = -10; i <= 10; i++)
-        {
-            for (int k = -10; k <= 10; k++)
-            {
-                if (snake.GetTile(new Vector3Int(i, k, 0)) != null)
-                {
 
-                    Debug.Log("i = " + i + " k = " + k);
-                }
-            }
-        }
-        */
-
-        InvokeRepeating("MyUpdate", 4.0f,0.3f);
+        spawnFruit();
+        InvokeRepeating("MyUpdate", 1.0f, 0.3f);
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if (!direction.Equals("left") && !direction.Equals("right") && Input.GetButtonDown("left"))
+        { 
+
+            direction = "left";
+            Invoke("MyUpdate", 0f);
+        }
+
+        else if (!direction.Equals("left") && !direction.Equals("right") && Input.GetButtonDown("right"))
+        {
+            direction = "right";
+            Invoke("MyUpdate", 0f);
+        }
+
+        else if (!direction.Equals("down") && !direction.Equals("up") && Input.GetButtonDown("down"))
+        {
+            direction = "down";
+            Invoke("MyUpdate", 0f);
+        }
+
+        else if (!direction.Equals("down") && !direction.Equals("up") && Input.GetButtonDown("up"))
+        {
+            direction = "up";
+            Invoke("MyUpdate", 0f);
+        }
+    }
 
     void MyUpdate()
     {
-        snake.SetTile(new Vector3Int(next.x, next.y, 0), snakePart);
+        //ce je to rob
+        if (snake.GetTile(new Vector3Int(next.x, next.y, 0)) == edge
+            || snake.GetTile(new Vector3Int(next.x, next.y, 0)) == snakePart)
+        {
+            EditorApplication.isPlaying = false;
+        }
 
-        Pozicija zac = (Pozicija)snqueue.Dequeue();
+        else if (fruitTable.GetTile(new Vector3Int(next.x, next.y, 0)) == fruit)
+        {
+            fruitTable.SetTile(new Vector3Int(next.x, next.y, 0), null);
+            snake.SetTile(new Vector3Int(next.x, next.y, 0), snakePart);
+            spawnFruit();
+        }
 
-        snake.SetTile(new Vector3Int(zac.x, zac.y, 0), null);
+        else
+        {
+            snake.SetTile(new Vector3Int(next.x, next.y, 0), snakePart);
+            Pozicija zac = (Pozicija)snqueue.Dequeue();
+        
+            snake.SetTile(new Vector3Int(zac.x, zac.y, 0), null);
+        }
+
+
 
         snqueue.Enqueue(next);
-        next = new Pozicija(next.x, next.y - 1);
+        if (direction.Equals("down"))
+        {
+            next = new Pozicija(next.x, next.y - 1);
+        }
+
+        else if (direction.Equals( "up"))
+        {
+            next = new Pozicija(next.x, next.y + 1);
+        }
+
+        else if (direction.Equals("left"))
+        {
+            next = new Pozicija(next.x - 1, next.y);
+        }
+
+        else
+        {
+            next = new Pozicija(next.x + 1, next.y);
+        }
+
+    }
+
+    void spawnFruit()
+    {
+        int zacx = (int)Random.Range(-12, 11);
+        int zacy = (int)Random.Range(-9, 8);
+        while (snake.GetTile(new Vector3Int(zacx, zacy, 0)) != null)
+        {
+            zacx = (int)Random.Range(-12, 11);
+            zacy = (int)Random.Range(-9, 8);
+        }
+        fruitTable.SetTile(new Vector3Int(zacx, zacy, 0), fruit);
     }
 
 }
